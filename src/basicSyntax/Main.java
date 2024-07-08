a06b3b5323bb759dfe798c4e8f9e3bdad93aa67f
Genki-Furumoto <g.furumoto.cingroup2012@gmail.com>
2024-07-08 10:47:48 +0900

    クラスについての課題

---
+++ src/basicSyntax/Main.java
@@ -0,0 +1,12 @@
+package basicSyntax;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		int age = 28;
+		double height = 179.5;
+		
+		GeekIntroduction.greeting(age, height);
+		GeekIntroduction.specialSkill(age,(int)height);
+	}
+ }
