d327a145ce36e0736b3a40b77d8fcfe8caacbbe3
Genki-Furumoto <g.furumoto.cingroup2012@gmail.com>
2024-07-09 10:21:20 +0900

    修正しました

--- src/basicSyntax/Main.java
+++ src/basicSyntax/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		int age = 28;
-		double height = 179.5;
+		double height = 301;
 		
 		GeekIntroduction.greeting(age, height);
 		GeekIntroduction.specialSkill(age,(int)height);
